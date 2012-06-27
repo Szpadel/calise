7438928b500e7506f9439d7fb1e3bee692e2bccd
smilzoboboz <smilzoboboz@users.sourceforge.net>
2012-06-27 19:00:29 +0200

    added git revision hash "tracking"

--- setup.py
+++ setup.py
@@ -13,13 +13,12 @@ from DistUtilsExtra.auto import setup
 from distutils.core import Extension
 
 
+# Reads file "infos.py" in that package and obtains from there the
+#
+# This "fake" class has been implemented because import priority between
+# root and workingDir modules varies from Distro to Distro.
+#
 class replacement():
-    """
-    Reads file "infos.py" in that package and obtains from there the
-
-    This "fake" class has been implemented because import priority between
-    root and workingDir modules varies from Distro to Distro.
-    """
     def __init__ (self, path ):
         self.__CapitalName__ = 'Calise'
         self.__LowerName__ = self.__CapitalName__.lower()
@@ -31,24 +30,29 @@ class replacement():
                         '='.join(line.split('=')[1:]).replace(' ', '')[1:-1])
 
 
+# Developement versions track
+#
+# Returns either:
+#  - SVN revision in the form SVN-XXXX, where XXXX is the revision number
+#  - GIT shorthash in the form GIT-xxxxxxx, where xxxxxxx are first 7 chars of
+#    sha1 hash
+#
+# Returns calise.infos.__version__ in no git nor svn exist
+#
 def get_svn_revision(path=None):
-    """
-    Returns the SVN revision in the form SVN-XXXX,
-    where XXXX is the revision number.
-
-    Returns SVN-unknown if anything goes wrong, such as an unexpected
-    format of internal SVN files.
-    """
     if path is None:
-        #path = os.path.dirname(os.path.realpath(__file__))
         baseName = sys.argv[0].replace("./","").\
             replace(os.path.basename(sys.argv[0]),"")
         path = os.path.join(os.getcwd(), baseName)
     infos = replacement(path)
-    rev = None
-    db_path = '%s/.svn/wc.db' % path
-
-    try:
+    # if there's git directory process as git
+    if os.path.isdir('%s/.git' % path):
+        gitRev = os.popen('git rev-parse --short HEAD')
+        gitRev = gitRev.read()[:-1]
+        versionString = u'GIT-%s' % gitRev
+    # else if there's svn directory process as subversion
+    elif os.path.isdir('%s/.svn' % path):
+        db_path = '%s/.svn/wc.db' % path
         with open(db_path) as fp:
             content = fp.read().replace("svn/ver/","\nsvn/ver/").split("\n")
         prog = re.compile('svn/ver/([0-9]+)/')
@@ -58,10 +62,10 @@ def get_svn_revision(path=None):
             if z is not None:
                 revisions.append(int(z.group(1)))
         rev = max(revisions)
-    except IOError:
-        pass
-    if rev:
         versionString = u'SVN-%s' % rev
+    else:
+        versionString = None
+    if versionString:
         with open(os.path.join(path, infos.__LowerName__, 'infos.py')) as fp:
             fc = fp.read()
             fc = fc.replace(
