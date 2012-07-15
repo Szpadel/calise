0a22238b4f2fd514e9e84c96bbe1edc69308db11
smilzoboboz <smilzoboboz@users.sourceforge.net>
2012-07-15 21:42:57 +0200

    fixed issue 3539286, has yet to be throughly tested but seems to work

--- bin/calised.py
+++ bin/calised.py
@@ -30,98 +30,7 @@ import dbus
 from calise.infos import __LowerName__
 from calise import optionsd as options
 from calise.dbusService import service
-
-
-class tempUtils():
-
-    def __init__(self):
-        self.workdir = None     # service tempdir (str)
-        self.pidfile = None     # service PID file (str)
-        self.pid = None         # service process ID (int)
-        self.uid = None         # service owner's user ID (int)
-
-    # read PID file (pf) and obtain service process ID
-    def getpidfromfile(self, pf=None):
-        retCode = 1
-        # input control
-        if pf is None and self.pidfile is not None:
-            pf = self.pidfile
-        elif pf is None and self.pidfile is None:
-            retCode = 2
-        # function body
-        if pf:
-            try:
-                with open(pf, 'r') as fp:
-                    pid = fp.read().splitlines()[0]
-                    pid = int(pid)
-                    self.pid = pid
-                    retCode = 0
-            except ValueError:
-                retCode = 3
-        return retCode
-
-    # obtain a possible service-tempdir and check if it's so
-    def obtainWorkdir(self, tempre='%s-' % __LowerName__):
-        retCode = 1
-        ext = '.pid'
-        for item in os.listdir(tempfile.gettempdir()):
-            pre_len = len(tempre)
-            if item[:pre_len] == tempre and len(item) == pre_len + 6:
-                workdir = os.path.join(tempfile.gettempdir(), item)
-                try:
-                    for item in os.listdir(workdir):
-                        if (
-                            item[:pre_len] == tempre and
-                            len(item) == pre_len + 6 + len(ext) and
-                            item[-len(ext):] == ext):
-                            pidfile = os.path.join(workdir, item)
-                            cc = self.getpidfromfile(pidfile)
-                            if 0 == cc:
-                                self.workdir = workdir
-                                self.pidfile = pidfile
-                                retCode = 0
-                            else:
-                                retCode = cc + 10
-                            break
-                except OSError as err:
-                    if err.errno == 13:
-                        retCode = 3
-        return retCode
-
-    # get PID's owner user ID
-    def getpuid(self, pid):
-        retCode = 1
-        # input control
-        if pid is None and self.pid is not None:
-            pid = self.pid
-        elif pid is None and self.pid is None:
-            retCode = 2
-        # function body
-        if pid:
-            try:
-                with open('/proc/%d/status' % pid, 'r') as fp:
-                    for ln in fp.readlines():
-                        if ln.startswith('Uid:'):
-                            uid = int(ln.split()[1])
-                            self.uid = uid
-            except IOError as err:
-                if err.errno == 2:
-                    pass
-        return retCode
-
-
-# function that clear calise tempdir eventually left behind from unexpected
-# program end
-def clearTempdir(pid):
-    print(
-        "%s: warning: PID found but no running service "
-        "instances." % sys.argv[0])
-    for tf in os.listdir(os.path.dirname(pid)):
-        os.remove(os.path.join(os.path.dirname(pid), tf))
-    os.rmdir(os.path.dirname(pid))
-    print(
-        "Folder %s has been removed will all its contents"
-        % os.path.dirname(pid))
+from calise.extServiceFunctions import getDbusSession, tempUtils, clearTempdir
 
 
 # Since all commands listed under serviceCommands make sense only with a
@@ -276,8 +185,49 @@ def mainService(kargs):
         os.rmdir(tempdir)
 
 
+def queryService(cbus, queryArgs, execArgs):
+    bus = cbus()
+    busObject = 'org.%s.service' % __LowerName__
+    busPath = '/org/%s/service' % __LowerName__
+    # try to connect to service DBus object
+    try:
+        service = bus.get_object(busObject, busPath)
+    except dbus.exceptions.DBusException as err:
+        if err.get_dbus_name() == \
+            'org.freedesktop.DBus.Error.ServiceUnknown':
+            return 21
+        else:
+            raise
+    # "set setting" command processing
+    for setting in options.settings:
+        ans = service.get_dbus_method('settingset', busObject)
+        try:
+            print ans(setting, options.settings[setting])
+        except dbus.exceptions.DBusException as err:
+            if err.get_dbus_name() == \
+                'org.freedesktop.DBus.Error.AccessDenied':
+                print(
+                    "warning: you are not allowed to set "
+                    "settings inside the service")
+    # "service execution" command processing
+    for command in queryArgs + execArgs:
+        ans = service.get_dbus_method(command, busObject)
+        try:
+            print ans()
+        except dbus.exceptions.DBusException as err:
+            if err.get_dbus_name() == \
+                'org.freedesktop.DBus.Error.AccessDenied':
+                print(
+                    "warning: you are not allowed to send "
+                    "command \"%s\" to the service" % command)
+            elif err.get_dbus_name() == \
+                'org.freedesktop.DBus.Python.IndexError':
+                print(
+                    "warning: there is no data to dump, retry later")
+    return 0
+
+
 def main():
-    retCode = 0
     # Arguments parse
     ap = options.serviceGetArgs(sys.argv[1:])
     ap.init_args()
@@ -295,83 +245,75 @@ def main():
         return 30
     # "normal" behaviour
     tu = tempUtils()
+
     tmp = tu.obtainWorkdir()
-    # client query
-    # if service is active, cli arguments are managed as execution commands
     if tmp == 0:
+        # valid tempdir found (means success!)
         # remove default profile setting since it cannot be changed during
         # service execution
         del options.settings['profile']
         # check if no arguments are being passed
         if not options.settings.keys() + ap.serviceArgs.keys():
-            print "error: a service should be already running"
+            print(
+                "%s: error: a service instance should be already running"
+                % sys.argv[0])
             return 6
         tu.getpuid(tu.pid)
-        # choose DBus bus between Session and System
-        # if process with pid doesn't exist (and so doesn't uid) means that
-        # process terminated unexpectedly and left behind temporary dir
         if tu.uid is None:
+            # process terminated without clearing
             clearTempdir(tu.pidfile)
             return 20
         elif tu.uid == 0:
+            # process started by root
             sbus = dbus.SystemBus
-        elif tu.uid == os.getuid() or os.getuid() == 0:
+        elif tu.uid == os.getuid():
+            # process owner is current user
             sbus = dbus.SessionBus
-            # root is able to control every service instance (whoever user
-            # started it)
-            if os.getuid() == 0:
+        elif tu.uid != 0 and os.getuid() == 0:
+            # process not started by root and current user is root
+            import pwd
+            user = pwd.getpwuid(tu.uid)[0]  # username of given UID
+            dbs = getDbusSession(user, os.getenv('DISPLAY'))
+            if dbs:
+                # DBus session found
+                os.environ['DBUS_SESSION_BUS_ADDRESS'] = dbs
                 os.setuid(tu.uid)
+                sbus = dbus.SessionBus
+            else:
+                # no DBus sessions found
+                return 40
         else:
+            # service started by another (non-root) user
             print(
                 "%s: error: the service was started by another user"
                 % sys.argv[0])
             return 22
-        # connection to the selected DBus bus
-        bus = sbus()
-        busObject = 'org.%s.service' % __LowerName__
-        busPath = '/org/%s/service' % __LowerName__
-        # try to connect to service DBus object
-        try:
-            service = bus.get_object(busObject, busPath)
-        except dbus.exceptions.DBusException as err:
-            if err.get_dbus_name() == \
-                'org.freedesktop.DBus.Error.ServiceUnknown':
-                clearTempdir(tu.pidfile)
-                return 21
-            else:
-                raise
-        # "set setting" command processing
-        for setting in options.settings:
-            ans = service.get_dbus_method('settingset', busObject)
-            try:
-                print ans(setting, options.settings[setting])
-            except dbus.exceptions.DBusException as err:
-                if err.get_dbus_name() == \
-                    'org.freedesktop.DBus.Error.AccessDenied':
-                    print(
-                        "warning: you are not allowed to set "
-                        "settings inside the service")
-        # "service execution" command processing
-        for command in ap.queryArgs.keys() + ap.execArgs.keys():
-            ans = service.get_dbus_method(command, busObject)
-            try:
-                print ans()
-            except dbus.exceptions.DBusException as err:
-                if err.get_dbus_name() == \
-                    'org.freedesktop.DBus.Error.AccessDenied':
-                    print(
-                        "warning: you are not allowed to send "
-                        "command \"%s\" to the service" % command)
-                elif err.get_dbus_name() == \
-                    'org.freedesktop.DBus.Python.IndexError':
-                    print(
-                        "warning: there is no data to dump, retry later")
-    # service start
-    # if there's no service active, cli arguments are managed as start
-    # options
-    elif tmp in (1,):
-        retCode = mainService(ap)
-    return retCode
+        # query DBus bus
+        qs = queryService(sbus, ap.queryArgs.keys(), ap.execArgs.keys())
+        if qs == 21:
+            clearTempdir(tu.pidfile)
+        return qs
+
+    elif tmp == 1:
+        # no temporary folders, start new service session
+        ms = mainService(ap)
+        return ms
+
+    elif tmp == 3:
+        # unable to access a probable tempdir
+        return tmp
+
+    elif tmp == 12:
+        # no pidfile inside checked workdir (should be impossible to obtain)
+        return tmp
+
+    elif tmp == 13:
+        # .pid file cannot contain a valid process ID (not a numeric integer)
+        return tmp
+
+    else:
+        # unexpected error code, quit please...
+        return tmp
 
 
 if __name__ == '__main__':
---
+++ calise/extServiceFunctions.py
@@ -0,0 +1,142 @@
+import os
+import sys
+import re
+import errno
+import tempfile
+
+from calise.infos import __LowerName__
+
+
+# get DBUS_SESSION_BUS_ADDRESS enviroment var from "outside"
+def getDbusSession(user, display=None):
+    retVal = None
+    displayProg = re.compile('.*\#[ a-zA-Z]+(:[0-9.]+).*', re.DOTALL)
+    busProg = re.compile('.*DBUS_SESSION_BUS_ADDRESS=([^\n]+)', re.DOTALL)
+    for bus in os.listdir('/home/%s/.dbus/session-bus/' % user):
+        with open('/home/%s/.dbus/session-bus/%s' % (user, bus)) as fp:
+            cont = fp.read()
+        a = displayProg.match(cont)
+        if a is not None:
+            if (
+                (not display and a.group(1).startswith(':0')) or
+                (a.group(1) == display)
+            ):
+                a = busProg.match(cont)
+                if a is not None:
+                    retVal = a.group(1)
+    return retVal
+
+
+# Manager for all temporary directory related things
+class tempUtils():
+
+    def __init__(self):
+        self.workdir = None     # service tempdir (str)
+        self.pidfile = None     # service PID file (str)
+        self.pid = None         # service process ID (int)
+        self.uid = None         # service owner's user ID (int)
+
+    def obtainWorkdir(self, tempre='%s-' % __LowerName__):
+        ''' obtain a possible service-tempdir and check if it's so
+
+        Return codes:
+              0: both "workdir" and "pidfile" class variables has been stored
+              1: generic error (there shouldn't be any tempdir)
+              3: file access error
+            >10: "getpidfromfile" function error (to get "getpidfromfile",
+                 subtract 10)
+        '''
+        retCode = 1
+        ext = '.pid'
+        for item in os.listdir(tempfile.gettempdir()):
+            pre_len = len(tempre)
+            if item[:pre_len] == tempre and len(item) == pre_len + 6:
+                workdir = os.path.join(tempfile.gettempdir(), item)
+                try:
+                    for item in os.listdir(workdir):
+                        if (
+                            item[:pre_len] == tempre and
+                            len(item) == pre_len + 6 + len(ext) and
+                            item[-len(ext):] == ext):
+                            pidfile = os.path.join(workdir, item)
+                            cc = self.getpidfromfile(pidfile)
+                            if 0 == cc:
+                                self.workdir = workdir
+                                self.pidfile = pidfile
+                                retCode = 0
+                            else:
+                                retCode = cc + 10
+                            break
+                except OSError as err:
+                    if err.errno == errno.EACCES:
+                        retCode = 3
+        return retCode
+
+    def getpidfromfile(self, pf=None):
+        ''' Read PID file (pf) and obtain service process ID
+
+        Return codes:
+              0: "pid" class variable has been stored
+              1: generic error
+              2: there's no pidfile to be used as source (not stored nor given)
+              3: file content cannot be a process id (not numeric integer)
+        '''
+        retCode = 1
+        # input control
+        if pf is None and self.pidfile is not None:
+            pf = self.pidfile
+        elif pf is None and self.pidfile is None:
+            retCode = 2
+        # function body
+        if pf:
+            try:
+                with open(pf, 'r') as fp:
+                    pid = fp.read().splitlines()[0]
+                    pid = int(pid)
+                    self.pid = pid
+                    retCode = 0
+            except ValueError:
+                retCode = 3
+        return retCode
+
+    def getpuid(self, pid):
+        ''' Get PID's owner user ID
+
+        Return codes:
+              0: "uid" class variable has been stored
+              1: genric error / requested process with id $pid is not running
+              2: there's no process id to check (not stored nor given)
+        '''
+        retCode = 1
+        # input control
+        if pid is None and self.pid is not None:
+            pid = self.pid
+        elif pid is None and self.pid is None:
+            retCode = 2
+        # function body
+        if pid:
+            try:
+                with open('/proc/%d/status' % pid, 'r') as fp:
+                    for ln in fp.readlines():
+                        if ln.startswith('Uid:'):
+                            uid = int(ln.split()[1])
+                            self.uid = uid
+                            retCode = 0
+            except IOError as err:
+                if err.errno == 2:
+                    pass
+        return retCode
+
+
+# function that clear calise tempdir eventually left behind from unexpected
+# program end
+def clearTempdir(pid):
+    print(
+        "%s: warning: PID found but no running service "
+        "instances." % sys.argv[0])
+    for tf in os.listdir(os.path.dirname(pid)):
+        os.remove(os.path.join(os.path.dirname(pid), tf))
+    os.rmdir(os.path.dirname(pid))
+    print(
+        "Folder %s has been removed will all its contents"
+        % os.path.dirname(pid))
