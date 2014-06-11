8b224c2e50277e71a16beb418659b8801dcaceac
smilzoboboz <smilzoboboz@users.sourceforge.net>
2014-06-11 11:52:38 +0200

    Camera module first adaptation
    Converted the code to the new modular concept.
    Cleaned/Improved messy parts of old code.
    Added per module calibration.

--- calise/thcamera.py
+++ calise/thcamera.py
@@ -1,4 +1,4 @@
-#    Copyright (C)   2011-2013   Nicolo' Barbon
+#    Copyright (C)   2011-2014   Nicolo' Barbon
 #
 #    This file is part of Calise.
 #
@@ -16,6 +16,7 @@
 #    along with Calise.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import sys
 import time
 import threading
 
@@ -23,32 +24,130 @@ from calise import pycamera
 from calise.infos import __LowerName__
 
 
-logger = logging.getLogger(".".join([__LowerName__, 'camera_thread']))
-
-
+# Module properties
+properties = {
+    'name': 'camera',
+    'type': 'input',
+    'description':
+        'Obtain ambient brightness values from any V4L2 compatible camera '
+        'device.',
+    'settings': {'device': {}, 'offset': 0},
+}
+# Events definition
 SendEvent = threading.Event()
 GetEvent = threading.Event()
 AbortEvent = threading.Event()
+logger = logging.getLogger(
+    ".".join([__LowerName__, '%s_thread' % properties['name']]))
+
+def get_info():
+    """ Get global module informations """
+    global properties
+    return properties
 
 
-class CameraThread(threading.Thread):
-    """ Camera Thread
-    
+class Configure():
+
+    def __init__(self):
+        self.th = _CameraThreadFunctions()
+        print(_("%s module configuration") % (properties['name'].capitalize()))
+
+    def conf_device(self):
+        """ Device sysfs informations setting
+
+        Set global properties['setting']['device'] to sysfs device
+        infos.
+
+        NOTE: Must be executed *before* conf_offset, otherwise the
+              latter won't have required data.
+        """
+        devices = self.th.get_devices()
+        default_index = 0
+        if len(devices) == 1:
+            index = default_index
+        elif len(devices) > 1:
+            # Print a list with all available devices
+            index = 0
+            for device in devices:
+                sys.stdout.write("%d: %s" % (index + 1, device['KERNEL']))
+                if list(device['ATTR'].keys()).count('name'):
+                    sys.stdout.write(" %s" % (device['ATTR']['name']))
+                sys.stdout.write("\n")
+                index += 1
+            # Ask the user to pick one of the devices previously listed
+            while True:
+                selection = raw_input(
+                    _("Choose one of cams listed above (default=%s): ") %
+                    devices[default_index]['KERNEL']))
+                if type(selection) != int and selection != '':
+                    print(_("Please retry and enter an integer."))
+                elif int(selection) > len(devices) or int(selection) < 1:
+                    print(_("Please retry and enter an integer within the "
+                            "valid range 1-%d.") % len(devices))
+                elif selection == '':
+                    index = default_index
+                    break
+                elif int(selection) <= len(devices) and int(selection) > 0:
+                    index = selection - 1
+                    break
+                sys.stdout.write("\n")
+        # Set global $device variable
+        global properties
+        properties['settings']['device'] = devices[index]
+
+    def conf_offset(self):
+        """ Device's brightness offset setting
+
+        Set global properties['settings']['offset'] to the minimum
+        possible brightness that can be captured from the device.
+
+        A lot of webcams never get 0/255 brightness value even with
+        white_balance, and similar controls turned off.
+        In most cases this doesn't mean that captured values are not
+        trustworthy but it just means that x/255 values are actually
+        x/255-offset values and correcting this offset lead to
+        precise capture values.
+
+        NOTE: Since every camera device gets different capture values
+              with similar brightness conditions, the actual precision
+              is not that relevant, you just need to have an input
+              device that give 0 > 255 values as requested to every
+              input modules.
+        """
+        self.th.initialize(properties['settings'])
+        raw_input(customWrap(
+            _("Cover the webcam and then press [ENTER] or [RETURN]")))
+        values = []
+        self.th.capture_start()
+        for x in range(40):
+            GetEvent.set()
+            brightness = self.th.set_brightness()
+            if brightness is not None:
+                values.append(brightness)
+                GetEvent.clear()
+        self.th.capture_stop()
+        global properties
+        properties['settings']['offset'] = min(values)
+
+
+class MainThread(threading.Thread):
+    """ Camera Main Thread
+
     This class is one of calise's core threads and communicates directly
     with the main thread.
-    
+
     After thread is initialized and started, a wait loop starts and the
     calling thread asks brightness values sending 'Get' events (as many
     as needed but just one at time)
-    
+
     Makes use of all three 'Get', 'Send' and 'Abort' global events.
-    
+
     """
-    
-    def __init__(self,device=None):
+
+    def __init__(self,settings):
         self.brightness = None
-        self.th = _CameraThreadFunctions(device)
-        threading.Thread.__init__(name="camera")
+        self.th = _CameraThreadFunctions(settings)
+        threading.Thread.__init__(name=properties['name'])
 
     def run(self):
         self.th.capture_start()
@@ -74,28 +173,33 @@ class CameraThread(threading.Thread):
 
 class _CameraThreadFunctions():
     """ calise.pycamera function wrapper
-    
-    This class is called only by "CameraThread" thread and cannot be
+
+    This class is called only by "MainThread" thread and shouldn't be
     accessed directly from outside.
-    
+
     Makes use of 'Get', and 'Abort' global events.
-    
+
     NOTE: this class puts on use "calise.pycamera" general functions
           specifically for calise needs. This should help for future
           individual calise.pycamera (core) code changes/upgrades.
     """
-    
-    def __init__(self,device=None):
+
+    def __init__(self,settings=None):
         self.camera_module = pycamera.CameraProcess()
-        self.camera_module.dev_init(device)
-    
+        if settings:
+            self.initialize(settings)
+
+    def initialize(self,settings):
+        self.camera_module.dev_init(settings['device']['KERNEL'])
+        self.offset = settings['offset']
+
     def capture_start(self):
         self.camera_module.start_capture()
         self.camera_module.set_ctrls()
         # first (buffered) frame to be discarded (see pycamera module
         # for more info about camera C-module behaviour)
         self.camera_module.get_frame_brightness()
-        
+
     def capture_stop(self):
         self.camera_module.reset_ctrls()
         self.camera_module.stop_capture()
@@ -103,14 +207,14 @@ class _CameraThreadFunctions():
 
     def set_brightness(self):
         """ Capture event handler
-        
+
         Waits for either a 'Get' or 'Abort' event to be set;
         'Abort' event takes over 'Proceed' one so that any pause/stop
         command has an almost immediate (but clean) effect.
-        
+
         Could not use "wait()" method since any "AbortEvent" called
         during the "wait()" timeout (if any) won't get caught.
-        
+
         """
         rc = True
         while rc is True:
@@ -118,7 +222,18 @@ class _CameraThreadFunctions():
                 rc = None
             elif GetEvent.is_set():
                 brightness = self.camera_module.get_frame_brightness()
-                rc = brightness
+                # normalize value as /255 removing any base offset
+                rc = (brightness - self.offset) / (255 - self.offset) * 255
             else:
                 time.sleep(0.0005)
         return rc
+
+    def get_controls(self):
+        """ Get brightness related device controls """
+        controls = self.camera_module.query_ctrls()
+        return controls
+
+    def get_devices(self):
+        """ Get a list of all v4l2 compatible devices """
+        devices = self.camera_module.get_available_devices()
+        return devices
